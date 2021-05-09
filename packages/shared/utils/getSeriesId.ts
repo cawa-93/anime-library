433a8870a2e656d3d63c2c8703afed1ab81eeb71
Alex Kozack <kozackunisoft@gmail.com>
2021-05-09 18:24:08 +0300

    feat: Поддержка ссылок с MAL

--- packages/shared/utils/getSeriesId.ts
+++ packages/shared/utils/getSeriesId.ts
@@ -1,3 +1,3 @@
 export function getSeriesId(str: string): `${number}` | undefined {
-  return /\/animes\/[a-z]*(?<animeID>[0-9]+)/.exec(str)?.groups?.animeID as `${number}`;
+  return /\/animes?\/[a-z]*(?<animeID>[0-9]+)/.exec(str)?.groups?.animeID as `${number}`;
 }
