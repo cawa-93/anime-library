10fc7a6ffed10929d861e07b4006a22e6e327954
Alex Kozack <cawa-93@users.noreply.github.com>
2021-06-02 11:40:20 +0300

    chore: Проверка типов в `pre-commit`
    Вместо проверки типов по всему проекту в `pre-push` теперь выполняется проверка в `pre-commit` и только в staged `.ts` и `.vue` файлах

---
+++ scripts/tsc-staged.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+staged=$(git diff --name-only --cached | grep -E "(tsconfig.json|.ts|.vue)$")
+
+args=()
+
+if [[ "$staged" == *"packages/main"* ]]; then
+  args=("${args[@]}" "npm:typecheck:main")
+fi
+
+if [[ "$staged" == *"packages/preload"* ]]; then
+  args=("${args[@]}" "npm:typecheck:preload")
+
+fi
+
+if [[ "$staged" == *"packages/renderer"* ]]; then
+  args=("${args[@]}" "npm:typecheck:renderer")
+fi
+
+# shellcheck disable=SC2128
+if [ -z "$args" ]; then
+  echo "No type sensitivity files staged"
+else
+  node scripts/buildEnvTypes.js
+  npx concurrently --raw "${args[@]}"
+fi
