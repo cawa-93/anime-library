1872fe31e098ca77d979c15041fcf78d5cabff67
Alex Kozack <cawa-93@users.noreply.github.com>
2021-06-16 16:45:25 +0300

    fix: Исправлена верстка плейлистов
    fixup 921522ea4aa34b1b90b8d652571cad91bf4c3b56

--- packages/renderer/src/components/WatchPage/PlayList.vue
+++ packages/renderer/src/components/WatchPage/PlayList.vue
@@ -8,20 +8,18 @@
       :key="item.id"
       href="#"
       :class="{active: item.id === selectedItemId}"
-      class="list-group-item"
+      class="list-group-item d-flex align-items-center"
       :title="item.title || ''"
       @click.prevent="$emit('item-click', item)"
     >
-      <span class="d-flex item-label text-truncate">
-        <span class="flex-fill d-inline-block text-truncate">{{ item.label }}</span>
-        <small
-          v-for="b of item.badges"
-          :key="b.text"
-          style="margin-left: 0.1em"
-          class="badge align-self-center"
-          :class="`bg-${b.style} ${['warning', 'info', 'light'].includes(b.style) ? 'text-dark' : ''}`"
-        >{{ b.text }}</small>
-      </span>
+      <span class="flex-fill d-inline-block text-truncate">{{ item.label }}</span>
+      <small
+        v-for="b of item.badges"
+        :key="b.text"
+        style="margin-left: 0.1em"
+        class="badge align-self-center"
+        :class="`bg-${b.style} ${['warning', 'info', 'light'].includes(b.style) ? 'text-dark' : ''}`"
+      >{{ b.text }}</small>
     </a>
   </nav>
 </template>
@@ -73,23 +71,15 @@ export default defineComponent({
 
 <style scoped>
 .list-group-item {
-  display: grid;
-  grid-template-rows: 1fr;
-  grid-template-columns: 25px 1fr;
   content-visibility: auto;
   contain-intrinsic-size: 24px;
 }
 
 .list-group-item:not(.active) {
   background: none;
-  color: inherit;
 }
 
 .list-group-item:hover:not(.active), .list-group-item:focus-visible:not(.active) {
   background-color: rgba(0, 0, 0, 0.05) !important;
-}
-
-.item-label {
-  grid-column-start: 2;
 }
 </style>
