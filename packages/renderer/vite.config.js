85a939ab045458557f78697d1007781d601b1d3f
Alex Kozack <cawa-93@users.noreply.github.com>
2021-06-16 11:30:09 +0300

    build(renderer): `quietDeps` для sass

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -79,6 +79,13 @@ export default defineConfig({
     },
     emptyOutDir: true,
   },
+  css: {
+    preprocessorOptions: {
+      sass: {
+        quietDeps: true,
+      },
+    },
+  },
   plugins: [
     vue(),
 
