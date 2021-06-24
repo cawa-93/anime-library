fe32199d17f78f56cd2874809ce2a8b0040a1b81
Alex Kozack <cawa-93@users.noreply.github.com>
2021-06-24 14:08:51 +0300

    perf: Рефакторинг функции `getFormattedVideoTime`

--- packages/renderer/src/utils/getFormattedVideoTime.ts
+++ packages/renderer/src/utils/getFormattedVideoTime.ts
@@ -1,13 +1,20 @@
 import {HOUR, SECOND_MS} from '/@/utils/time';
 
 
+const MIN_SEC = new Intl.DateTimeFormat('ru', {
+  minute: 'numeric',
+  second: 'numeric',
+  timeZone: 'UTC',
+});
+
+const HOUR_MIN_SEC = new Intl.DateTimeFormat('ru', {
+  minute: 'numeric',
+  second: 'numeric',
+  hour: 'numeric',
+  timeZone: 'UTC',
+});
+
+
 export function getFormattedVideoTime(seconds: number): string {
-  const d = new Date(seconds * SECOND_MS);
-
-  const options: Intl.DateTimeFormatOptions = {minute: 'numeric', second: 'numeric', timeZone: 'UTC'};
-  if (seconds >= HOUR) {
-    options.hour = 'numeric';
-  }
-
-  return d.toLocaleTimeString('ru', options);
+  return (seconds >= HOUR ? HOUR_MIN_SEC : MIN_SEC).format(seconds * SECOND_MS);
 }
