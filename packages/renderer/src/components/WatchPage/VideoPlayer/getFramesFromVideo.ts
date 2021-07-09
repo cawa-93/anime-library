a3d615b474bca32b5cd8f20e3c6088ce9263463e
Alex Kozack <cawa-93@users.noreply.github.com>
2021-07-09 12:02:36 +0300

    chore: Удалены отладочные логи

--- packages/renderer/src/components/WatchPage/VideoPlayer/getFramesFromVideo.ts
+++ packages/renderer/src/components/WatchPage/VideoPlayer/getFramesFromVideo.ts
@@ -11,51 +11,27 @@ export async function* getFramesFromVideo(timestamps: number[], videoSource: str
     throw new Error('Не удалось создать 2d контекст для canvas для отрисовки привью видео-таймлайна');
   }
 
-  const stat: { render: number[], seek: number[] } = {
-    render: [],
-    seek: [],
-  };
-
-  const loadedmetadata = new Promise(r => video.onloadedmetadata = r);
   const getFrame = (time: number) => new Promise<string>(resolve => {
-
-    let start = performance.now();
-
     video.onseeked = () => {
-
-      stat.seek.push(performance.now() - start);
-      start = performance.now();
-
       ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-
-      stat.render.push(performance.now() - start);
-
       resolve(canvas.toDataURL());
     };
     video.currentTime = time;
   });
 
-  for (const timestamp of timestamps) {
-    await loadedmetadata;
-    canvas.height = video.videoHeight;
-    canvas.width = video.videoWidth;
+  /**
+   * Нужно дождаться когда загрузятся метаданные видео, чтобы определить его размеры и изменить размеры canvas
+   */
+  await new Promise(r => video.onloadedmetadata = r);
 
+  canvas.height = video.videoHeight;
+  canvas.width = video.videoWidth;
+
+  for (const timestamp of timestamps) {
     yield {
       time: timestamp,
       data: await getFrame(timestamp),
     };
   }
-
-  const renderTotal = stat.render.reduce((s, i) => s + i, 0);
-  const seekTotal = stat.seek.reduce((s, i) => s + i, 0);
-
-  const statMsg = [
-    `Seek total time: ${seekTotal}`,
-    `Seek avg time: ${seekTotal / stat.seek.length}`,
-    `Render total time: ${renderTotal}`,
-    `Render avg time: ${renderTotal / stat.render.length}`,
-  ].join('\n');
-
-  console.info(statMsg);
 }
 
