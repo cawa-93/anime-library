28b9d21bb3b436c4020a450715276b75737990ba
Alex Kozack <cawa-93@users.noreply.github.com>
2021-08-20 12:27:11 +0300

    test: Fix tests

--- packages/renderer/src/utils/videoProvider/providers/anime365/anime365.spec.ts
+++ packages/renderer/src/utils/videoProvider/providers/anime365/anime365.spec.ts
@@ -1,10 +1,11 @@
-import expectedData from 'src/utils/videoProvider/providers/anime365/episodes-filter-data';
+import expectedData from '/@/utils/videoProvider/providers/anime365/episodes-filter-data';
 import {resolveEpisodesList} from '/@/utils/videoProvider/providers/anime365/resolveEpisodesList';
 
 
 describe('Фильтр эпизодов', () => {
   for (const {originalSeries, expectedEpisodes} of expectedData) {
-    it('Фтльтр для ' + originalSeries.seriesId, () => {
+    it('Фильтр для ' + originalSeries.seriesId, () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       const resolved = resolveEpisodesList(originalSeries as any, new Map);
       expect(resolved.map((e: { id: number }) => e.id)).toEqual(expectedEpisodes);
     });
