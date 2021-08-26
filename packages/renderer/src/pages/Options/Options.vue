896374318337610121c9eb2d42af477d41c055c7
Alex Kozack <cawa-93@users.noreply.github.com>
2021-08-26 11:22:00 +0300

    feat: Выделение секций параметров при навигации
    При переходе по ссылке навигации на странице настроек целевая секция с параметрами будет потряхиваться для вузуального выделения ГДЕ она.

--- packages/renderer/src/pages/Options/Options.vue
+++ packages/renderer/src/pages/Options/Options.vue
@@ -37,66 +37,66 @@ const appVersion = import.meta.env.VITE_APP_VERSION;
           </nav>
         </div>
         <div class="col">
-          <h2 id="connections">
-            Подключене аккаунтов
-          </h2>
-          <option-anime365 class="my-3" />
-          <shiki-oauth class="my-3" />
-
-          <h2
-            id="app-settings"
-            class="mt-4"
-          >
-            Настройки приложения
-          </h2>
-          <section class="card my-3">
-            <p class="card-header">
-              Настройки видеоплеера
-            </p>
-            <p class="card-body mb-0">
-              <timeline-thumbnails />
-            </p>
+          <section id="connections">
+            <h2>
+              Подключене аккаунтов
+            </h2>
+            <option-anime365 class="my-3" />
+            <shiki-oauth class="my-3" />
           </section>
 
-          <section class="card my-3">
-            <p class="card-header">
-              Тема приложения
-            </p>
-            <p class="card-body mb-0">
-              <color-scheme />
-            </p>
+          <section id="app-settings">
+            <h2 class="mt-4">
+              Настройки приложения
+            </h2>
+            <section class="card my-3">
+              <p class="card-header">
+                Настройки видеоплеера
+              </p>
+              <p class="card-body mb-0">
+                <timeline-thumbnails />
+              </p>
+            </section>
+
+            <section class="card my-3">
+              <p class="card-header">
+                Тема приложения
+              </p>
+              <p class="card-body mb-0">
+                <color-scheme />
+              </p>
+            </section>
           </section>
 
 
-          <h2
-            id="help"
-            class="mt-4"
-          >
-            Справка
-          </h2>
-          <section class="card my-3">
-            <p class="card-header">
-              О приложении
-            </p>
-            <p class="card-body mb-0">
-              Версия: <strong>{{ appVersion }}</strong>
-              <br>
-              Исходный код на GitHub:
-              <a
-                href="#"
-                @click.prevent="openGitHub()"
-              >cawa-93/anime-library</a>
-              <br>
-              Сообщество пользователей в
-              <a
-                href="#"
-                @click.prevent="openVK"
-              >Вконтакте</a> и в
-              <a
-                href="#"
-                @click.prevent="openTG"
-              >Telegram</a>
-            </p>
+          <section id="help">
+            <h2 class="mt-4">
+              Справка
+            </h2>
+            <section class="card my-3">
+              <p class="card-header">
+                О приложении
+              </p>
+              <p class="card-body mb-0">
+                Версия: <strong>{{ appVersion }}</strong>
+                <br>
+                Исходный код на GitHub:
+                <a
+                  href="#"
+                  @click.prevent="openGitHub()"
+                >cawa-93/anime-library</a>
+                <br>
+                Сообщество пользователей в
+                <a
+                  href="#"
+                  @click.prevent="openVK"
+                >Вконтакте</a> и в
+                <a
+                  href="#"
+                  @click.prevent="openTG"
+                >Telegram</a>
+              </p>
+            </section>
           </section>
         </div>
       </div>
@@ -106,4 +106,29 @@ const appVersion = import.meta.env.VITE_APP_VERSION;
 
 
 <style scoped>
+/**
+ * Анимация взята со страницы
+ * https://css-tricks.com/snippets/css/shake-css-keyframe-animation/
+ */
+section:target {
+  animation: shake 0.82s cubic-bezier(.36, .07, .19, .97) both;
+}
+
+@keyframes shake {
+  10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
 </style>
