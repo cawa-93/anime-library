8a4e75ef1531f4a367092ee4c2984fa2df69f689
Alex Kozack <cawa-93@users.noreply.github.com>
2021-12-11 23:32:02 +0200

    chore: Рефакторинг диалогов

--- packages/main/src/ipc/DialogsHost.ts
+++ packages/main/src/ipc/DialogsHost.ts
@@ -1,16 +1,17 @@
-import type {Dialog} from 'electron';
+import type {Dialog, IpcMainInvokeEvent} from 'electron';
 import {dialog, ipcMain} from 'electron';
 import {IpcChannels} from '/@shared/ipcChannels';
 
 
 
-ipcMain.handle(IpcChannels.Dialog, (_, method: keyof Dialog, ...args: any[]) => {
-  const validateMethod = (m: keyof typeof dialog): m is keyof Electron.Dialog => typeof dialog[m] === 'function';
-
-  if (!validateMethod(method)) {
-    throw new TypeError(`Electron.Dialog.${method} is not a function`);
+function listener(_: IpcMainInvokeEvent, method: keyof Dialog, ...args: unknown[]) {
+  if (method === 'showErrorBox') {
+    const [title, content] = args as Parameters<Dialog[typeof method]>;
+    return dialog.showErrorBox(title, content);
   }
 
-  // @ts-expect-error Предотвращение ошибки типов выполняется шагом ранее в рантайме
-  return dialog[method](...args);
-});
+  throw new TypeError(`Electron.Dialog.${method} method is not implemented`);
+}
+
+
+ipcMain.handle(IpcChannels.Dialog, listener);
