d11e34d5c2bf4609b06ebc1e60a94c7855f24c49
Alex Kozack <cawa-93@users.noreply.github.com>
2022-02-21 18:55:29 +0200

    Прототип настройки зеркала для anime365

--- packages/renderer/src/pages/Options/OptionsAnime365/OptionsAnime365.vue
+++ packages/renderer/src/pages/Options/OptionsAnime365/OptionsAnime365.vue
@@ -3,6 +3,7 @@ import {TabList, TabGroup, Tab, TabPanel, TabPanels} from '@headlessui/vue';
 import OptionsAnime365Status from '/@/pages/Options/OptionsAnime365/OptionsAnime365Status.vue';
 import OptionsAnime365Token from '/@/pages/Options/OptionsAnime365/OptionsAnime365Token.vue';
 import OptionsAnime365Login from '/@/pages/Options/OptionsAnime365/OptionsAnime365Login.vue';
+import OptionsAnime365Mirror from '/@/pages/Options/OptionsAnime365/OptionsAnime365Mirror.vue';
 
 defineEmits({
   'save': null,
@@ -45,11 +46,24 @@ defineEmits({
           Шаг 2
         </button>
       </Tab>
+
+      <Tab
+        v-slot="{ selected }"
+        as="template"
+      >
+        <button
+          class="btn"
+          :class="{'bg-accent': selected}"
+        >
+          Обход блокировки
+        </button>
+      </Tab>
     </TabList>
     <TabPanels>
       <TabPanel><options-anime365-status /></TabPanel>
       <TabPanel><options-anime365-login /></TabPanel>
       <TabPanel><options-anime365-token @save="$emit('save')" /></TabPanel>
+      <TabPanel><options-anime365-mirror /></TabPanel>
     </TabPanels>
   </TabGroup>
 </template>
---
+++ packages/renderer/src/pages/Options/OptionsAnime365/OptionsAnime365Mirror.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+import {ref} from 'vue';
+
+
+const newDomain = ref(localStorage.getItem('anime365-mirror') || '');
+
+const onSubmitHandler = () => {
+  localStorage.setItem('anime365-mirror', newDomain.value);
+};
+</script>
+
+<template>
+  <form @submit.prevent="onSubmitHandler">
+    <label for="anime365-domain">Домен:</label>
+    <input
+      id="anime365-domain"
+      v-model="newDomain"
+      type="url"
+      placeholder="https://"
+      list="anime365-domains-list"
+    >
+
+
+    <datalist id="anime365-domains-list">
+      <option value="https://smotret-anime.com/" />
+      <option value="https://anime365.ru/" />
+      <option value="https://smotret-anime.online/" />
+    </datalist>
+
+    <button class="btn bg-accent">
+      Сохранить
+    </button>
+  </form>
+</template>
+
+<style scoped>
+form {
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-template-columns: 1fr auto;
+  grid-template-rows: auto auto;
+  gap: 5px 10px;
+}
+label { grid-area: 1 / 1 / 2 / 3; }
+input { grid-area: 2 / 1 / 3 / 2; }
+button { grid-area: 2 / 2 / 3 / 3; }
+</style>
