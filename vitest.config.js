4aa0e3acb31fa2a9712424ffa7de8c77e25983ac
Alex Kozack <cawa-93@users.noreply.github.com>
2022-02-22 13:29:30 +0200

    chore: Увеличен лимит времени на тесты

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
+    testTimeout: 50_000,
+    hookTimeout: 50_000,
     include: ['./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
